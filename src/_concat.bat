62bc97f838bf3d08504ddadc07d75592d2987e6e
BdR76 <bdr1976@gmail.com>
2021-12-28 14:19:59 +0100

    Run lcdgame.js locally
    Add files and configuration to be able to run lcdgame.js locally from C:\

---
+++ src/_concat.bat
@@ -0,0 +1,23 @@
+REM // LCD game JavaScript library
+REM // Bas de Reuver (c)2018
+
+del ..\bin\lcdgame.js
+
+echo /* LCD game JavaScript library -- by BdR 2018 */>> ..\bin\lcdgame.js
+echo.>> ..\bin\lcdgame.js
+
+type System.js >> ..\bin\lcdgame.js
+type State.js >> ..\bin\lcdgame.js
+type StateManager.js >> ..\bin\lcdgame.js
+type AnimationFrame.js >> ..\bin\lcdgame.js
+
+
+type Menu.js >> ..\bin\lcdgame.js
+type Highscores.js >> ..\bin\lcdgame.js
+
+type Game.js >> ..\bin\lcdgame.js
+type Shape.js >> ..\bin\lcdgame.js
+type Button.js >> ..\bin\lcdgame.js
+type Timer.js >> ..\bin\lcdgame.js
+
+REM // then js compress using https://jscompress.com/
